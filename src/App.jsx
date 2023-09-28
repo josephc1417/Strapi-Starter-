7c2c5537b6e5a78bd044800d90138a49bb292cd9
nova vita <42131370+josephc1417@users.noreply.github.com>
2023-09-28 18:38:06 -0400

    testing state

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
+import { useGlobalContext } from "./contex";
+
+
+
 const App = () => {
+  const {isSidebarOpen}=useGlobalContext();
+  console.log(isSidebarOpen);
   return <h2>Strapi Starter</h2>;
 };
 export default App;
