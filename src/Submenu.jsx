2cf8143d5653b32182e6255c21552dfa66bc5b3d
nova vita <42131370+josephc1417@users.noreply.github.com>
2023-10-05 23:12:39 -0400

    working on the submenue

--- src/Submenu.jsx
+++ src/Submenu.jsx
@@ -1,8 +1,18 @@
+import { useGlobalContext } from "./Context"
+import sublinks from "./data"
+
+
 
 const Submenu = () => {
+  const {pageID} = useGlobalContext()
+  const page = sublinks.find((link) => link.pageId === pageID)
+  console.log(page)
   return (
-    <div>
-        <h2>Submenu</h2>      
+    <div className="submenu1">
+        <h5>{page?.page}</h5>
+        <div className="submenu-links" style={{gridTemplateColumns:page?.links?.length> 3 ? '1fr 1fr' : '1fr'}}>
+         
+
     </div>
   )
 }
